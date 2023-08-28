3a79978341bb8254acbf31be52d48f3083a72df3
Rolf Schmidt <rolf@schmidt.ink>
2023-08-28 10:27:07 +0200

    Update zammad_new_szpm.rb

--- zammad_new_szpm.rb
+++ zammad_new_szpm.rb
@@ -1,8 +1,10 @@
 require 'json'
 require 'base64'
 
-File.write("#{ARGV[0]}/#{ARGV[1].gsub(/([^-])([A-Z])/, '\1_\2').downcase}.szpm", %Q[{
-  "name": "#{ARGV[1]}",
+name = ARGV[0].split('/')[-1]
+
+File.write("#{ARGV[0]}/#{name.gsub(/([^-])([A-Z])/, '\1_\2').downcase}.szpm", %Q[{
+  "name": "#{name}",
   "version": "1.0.0",
   "vendor": "Example GmbH",
   "license": "GNU AFFERO GENERAL PUBLIC LICENSE",
