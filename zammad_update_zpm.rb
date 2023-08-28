d24cd7b5ca48fd8a30fbb28f098a6e6707478f9b
Rolf Schmidt <rolf@schmidt.ink>
2023-08-28 10:10:59 +0200

    Create zammad_update_zpm.rb

---
+++ zammad_update_zpm.rb
@@ -0,0 +1,10 @@
+require 'json'
+
+files = Dir["#{ARGV[0]}/**/*.*"].select{|f| f !~ /.*\.(md|szpm)/ }.map {|f| { location: f.sub("#{ARGV[0]}/", ''), permission: 644 } }
+
+szpm_file = Dir["#{ARGV[0]}/*.szpm"].first
+
+szpm = JSON.parse(File.read(szpm_file))
+szpm['files'] = files
+
+File.write(szpm_file, "#{JSON.pretty_generate(szpm)}\n")
