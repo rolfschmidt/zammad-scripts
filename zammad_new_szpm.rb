f634f1e0e1e60c3987f9688ade0f3125079abb26
Rolf Schmidt <rolf.schmidt@zammad.com>
2023-09-08 08:18:44 +0100

    improve output for new szpm

--- zammad_new_szpm.rb
+++ zammad_new_szpm.rb
@@ -1,11 +1,11 @@
 require "#{__dir__}/zammad_helper.rb"
 
 name = ARGV[0].split('/')[-1]
-filename = name.szpm_name
+filename = "#{ARGV[0]}/#{name.szpm_name}.szpm"
 
-puts "create zzpm #{filename}..."
+puts "create new szpm #{filename}..."
 
-File.write("#{ARGV[0]}/#{filename}.szpm", %Q[{
+File.write(filename, %Q[{
   "name": "#{name}",
   "version": "1.0.0",
   "vendor": "Example GmbH",
